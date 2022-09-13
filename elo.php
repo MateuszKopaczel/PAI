c62da96832d2ea45be8d76c1f1b4a27def019598
MateuszKopaczel <matibru74@gmail.com>
2022-09-13 12:50:59 +0200

    Hello World

---
+++ elo.php
@@ -0,0 +1,34 @@
+<style>
+    p{
+        color: pink;
+    }
+</style>
+<html>
+    
+<head> 
+    
+   <meta charset="UTF-8">
+
+</head>
+<body>
+<?php
+
+$e = 1;
+echo "<p>Hello word <br>;
+My counter $e <br>;
+ Hello Friends</p>";
+
+
+?>
+
+
+
+
+</body>
+
+
+
+
+
+
+</html>
