fb8959db37401634effd9fc31246a569c8f63066
MateuszKopaczel <matibru74@gmail.com>
2022-09-14 15:43:33 +0200

    komyuuy

--- elo.php
+++ elo.php
@@ -1,4 +1,5 @@
 <style>
+    
     p{
         color: pink;
     }
@@ -8,17 +9,26 @@
 <head> 
     
    <meta charset="UTF-8">
-
 </head>
 <body>
+
 <?php
+/*
 
 $e = 1;
 echo "<p>Hello word <br>;
 My counter $e <br>;
  Hello Friends</p>";
+*/
 
-
+$liczba = 0;
+if($liczba == 0){
+echo "niemasz wiadomosci" ;
+}
+else
+{
+    echo "masz $liczba wiadomosci " ;
+}
 ?>
 
 
