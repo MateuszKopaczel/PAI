795aa09763f72b39fceff835aa37ac5ab3fd1cba
MateuszKopaczel <91668637+MateuszKopaczel@users.noreply.github.com>
2022-11-15 15:38:23 +0100

    Add files via upload

--- zadanie.php
+++ zadanie.php
@@ -1,22 +1,21 @@
 <?php
+$hostname = 'localhost'; 
+$database = 'paiproject'; 
+$username = 'root'; 
+$password = ''; 
 
-for($wys=1; $wys<=5; $wys++) {
+$conn = mysqli_connect($hostname, $username, $password, $database);
 
-    for($szer=1; $szer<=4; $szer++) {
-        if($wys==1 || $wys==5) {
-            echo "*";
-        }
-        elseif($szer==1 || $szer==4) {
-            echo "*";
-        }
-        else {
-            echo "_";
-        }
-        
-    }
-    echo "<br>";
+$query = "SELECT imie,nazwisko,pesel FROM tabela";
+$result = mysqli_query($conn, $query);
+
+
+echo "<ol>";
+while($row = mysqli_fetch_row($result)){
+    echo "<li>";
+    echo $row[0]. ' '.$row[1]. ' '.$row[2]."<br>";
+    echo "</li>";
 }
-
-
+echo "</ol>";
 
 ?>
