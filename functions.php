dc3f39b870211732323e31ac97a0ff6f03258bdd
MateuszKopaczel <91668637+MateuszKopaczel@users.noreply.github.com>
2022-12-07 16:24:15 +0100

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,83 @@
+<?php
+
+
+// połączenie z bazą danych
+$serverName = "localhost";
+$userName = "root";
+$password = '';
+$db = "project";
+
+
+$conn = mysqli_connect($serverName, $userName, $password, $db);
+
+
+function addToDB($login, $psw)
+{
+    // dodanie nowego użytkownika do bazy danych
+    global $conn;
+    $query = "SELECT * FROM users WHERE uname= '$login'";
+    $result = $conn->query($query);
+    $asd = $result->fetch_array(MYSQLI_ASSOC);
+    if ($asd == null) {
+        $hashpsw = sha1($psw);
+        $sql = "INSERT INTO `users` (`ID`, `uname`, `passwd`) VALUES (NULL, '$login', '$hashpsw')";
+
+        if ($result = $conn->query($sql)) {
+            echo "Zarejestrowano pomyślnie";
+        } else {
+            echo "Nie udało się zarejestrować";
+        }
+    }else{echo "Już istnieje taki użytkownik!";}
+}
+
+function passChecker($passwd, $uname)
+{
+    global $conn;
+    // sprawdzenie poprawoności hasła w bazie danych
+    $query = "SELECT * FROM users WHERE uname = '$uname' ";
+    $result = $conn->query($query);
+    if ($result != null) {
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        if (sha1($passwd) == $row['passwd']) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        echo "XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD";
+        return false;
+    }
+}
+
+
+function showMonument($name)
+{
+    global $conn;
+// Wyświetlanie z bazy danych komentarzy
+    $query = "SELECT * FROM monuments WHERE monument = '$name' ";
+    $result = $conn->query($query);
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    echo "<h1>" . $row['monument'] . "</h1>";
+    echo "<p>" . $row['info'] . "</p>";
+    echo "<img src='" . $row['img'] . "' width=250px>";
+}
+
+function addComment($name, $surname, $message)
+{
+    global $conn;
+    // dodawanie komentarza do bazy danych
+    $sql = "INSERT INTO `comments` (`ID`, `name`, `surname`, `message`) VALUES (NULL, '$name', '$surname', '$message')";
+
+    if ($result = $conn->query($sql)) echo "Dodano nowy rekord";
+    else echo "Nie udało się dodać nowego rekordu";
+}
+
+function showComment()
+{
+    global $conn;
+    $query = "SELECT * FROM comments";
+    $result = mysqli_query($conn, $query);
+    while ($row = mysqli_fetch_row($result)) {
+        echo "<h3>" . $row[1] . " " . $row[2] . "</h3> <p>" . $row[3] . "</p>";
+    }
+}
---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+
+if (isset($_GET["page"])) {
+    $strona = $_GET["page"];
+} else {
+    $strona = "main";
+}
+include("functions.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-inverse">
+        <section class="container-fluid">
+            <section class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Logo</a>
+            </section>
+            <section class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php?page=main">Strona Główna</a></li>
+                    <li><a href="index.php?page=monuments">Monumenty</a></li>
+                    <li><a href="index.php?page=contact">Kontakt</a></li>
+                    <li><a href="index.php?page=map">Mapa</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.php?page=login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><a href="index.php?page=rejestracja"><span class="glyphicon glyphicon-log-in"></span> Rejestracaj</a></li>
+                </ul>
+            </section>
+        </section>
+    </nav>
+
+    <?php
+    if ($strona != null){include($strona . '.php');}
+?>
+
+    <footer class="container-fluid text-center">
+        <p>Footer</p>
+    </footer>
+</body>
